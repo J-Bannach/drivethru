c2fb2f1777b728a532ad365e534324655c752d82
J-Bannach <JohannaBannach@gmail.com>
2021-05-11 14:48:52 +0200

    ➕ Add inquirer

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-import fs from "fs";
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log(fs.readFileSync("./README.md", "utf-8"));
+// readline.question(`What's your name?`, (name: string) => {
+//   console.log(`Hi ${name}!`);
+//   readline.close();
+// });
+
+readline.question(`Which password do you want to use?`, (password: string) => {
+  readline.question(`What do you want to do with it?`, (command: string) => {
+    console.log(`Please ${command} this password: ${password}`);
+    readline.close();
+  });
+});
