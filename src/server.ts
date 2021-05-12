9609d3f22319318f7c0339b86548bf3dc4a3fff1
J-Bannach <JohannaBannach@gmail.com>
2021-05-12 17:53:38 +0200

    :sparkles: Implement read from db.json function

--- src/server.ts
+++ src/server.ts
@@ -6,22 +6,32 @@ import {
 } from "./utils/questions";
 import { isMainPasswordValid } from "./utils/validation";
 import { printPassword } from "./utils/messages";
+import { readCredentials } from "./utils/credentials";
 
 // function start() {
 const start = async () => {
   /* Solution with while */
   let mainPassword = await askForMainPassword();
-  while (!isMainPasswordValid(mainPassword)) {
+  while (!(await isMainPasswordValid(mainPassword))) {
     console.log("Is invalid");
     mainPassword = await askForMainPassword();
   }
   console.log("Is valid");
 
   const command = await chooseCommand();
+
   switch (command) {
     case "list":
       {
-        const service = await chooseService(["Google", "GitHub", "Instagram"]);
+        const credentials = await readCredentials();
+        const credentialServices = credentials.map(
+          (credential) => credential.service
+        );
+        const service = await chooseService(credentialServices);
+        const selectedService = credentials.find(
+          (credential) => credential.service === service
+        );
+        console.log(selectedService);
         printPassword(service);
       }
       break;
