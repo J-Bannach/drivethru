9ecbd534b9f968682931236bbd287c7140f1f91c
J-Bannach <JohannaBannach@gmail.com>
2021-05-12 16:46:13 +0200

    :bug: Reinstall crypto-js

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,8 +1,9 @@
 import fs from "fs/promises";
+import sha256 from "crypto-js/sha256";
 
 export const isMainPasswordValid = async (
   plaintextPassword: string
 ): Promise<boolean> => {
   const passwordHash = await fs.readFile("./.password", "utf-8");
-  return plaintextPassword === passwordHash;
+  return sha256(plaintextPassword).toString() === passwordHash;
 };
